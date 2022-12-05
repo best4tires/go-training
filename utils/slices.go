793e5784c5a14a3f46a76c3655c68ce7d9faa573
Martin Gilles <martin.gilles@gundlach-automotive.com>
2022-12-05 14:15:01 +0100

    add eps-equal

--- utils/slices.go
+++ utils/slices.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"math"
 	"reflect"
 	"unsafe"
 )
@@ -15,4 +16,8 @@ func SlicesPtrEqual[T any](ts1 []T, ts2 []T) bool {
 	h1 := (*reflect.SliceHeader)(unsafe.Pointer(&ts1))
 	h2 := (*reflect.SliceHeader)(unsafe.Pointer(&ts2))
 	return h1.Data == h2.Data
+}
+
+func EpsEqual(eps float64, f1, f2 float64) bool {
+	return math.Abs(f1-f2) < eps
 }
